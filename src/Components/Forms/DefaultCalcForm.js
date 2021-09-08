b85607d0696bd87073f5c48346cdf47f993dc83d
horiguchi <horiguchi@sannoweb.info>
2021-09-08 22:46:36 +0900

    フォーム部分レイアウト

--- src/Components/Forms/DefaultCalcForm.js
+++ src/Components/Forms/DefaultCalcForm.js
@@ -1,23 +1,27 @@
 import {
     TextField,
     Button,
-    Stack
+    Stack,
+    FormLabel
 } from '@mui/material';
+import { TextItem } from '../Styleds/Box.js'
 
-  
+
 const DefaultCalcForm = () => {
     return (
-        <Stack
-        component="form"
-        sx={{
-          '& > :not(style)': { m: 2, width: '25ch', textAlign: 'center'},
-        }}
-        noValidate
-        autoComplete="off">
-            <TextField size="small" />
-            <TextField size="small" />
-            <Button>計算するよ</Button>
+        <div>
+        <Stack component="form" autoComplete="off">
+            <TextItem mx="auto">
+                <FormLabel>ナビダイヤル通話時間</FormLabel>
+                <TextField size="small" />
+            </TextItem>
+            <TextItem mx="auto">
+                <FormLabel>10秒毎の料金</FormLabel>
+                <TextField size="small" />
+            </TextItem>
+            <Button mx="auto">計算するよ</Button>
         </Stack>
+        </div>
     );
 };
 
