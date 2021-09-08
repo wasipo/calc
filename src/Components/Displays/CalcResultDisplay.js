8a3d57d69ef3a96a402167fc269d4d1155fc1459
horiguchi <horiguchi@sannoweb.info>
2021-09-08 22:45:45 +0900

    directory構成変更

--- src/Components/Displays/CalcResultDisplay.js
+++ src/Components/Displays/CalcResultDisplay.js
@@ -1,17 +1,6 @@
 import {
     Box,
-    Grid,
-    Paper,
 } from '@mui/material';
-import { styled } from '@mui/material/styles';
-
-
-const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
 
 const CalcResultDisplay = () => {
     return (
