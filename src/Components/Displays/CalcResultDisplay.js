1eefcfff250562d8928a362d5e7fb22c5e873cd5
horiguchi <horiguchi@sannoweb.info>
2021-09-11 13:59:12 +0900

    Formの結果をPropsで別のコンポーネントに送る

--- src/Components/Displays/CalcResultDisplay.js
+++ src/Components/Displays/CalcResultDisplay.js
@@ -2,11 +2,13 @@ import {
     Box,
 } from '@mui/material';
 
-import {ViewResult} from '../Styleds/TypoGraphy';
+import {ViewResult} from '../Styleds/TypoGraphy'
+import {RegisterForm} from 'react-hook-form'
 
 
 const CalcResultDisplay = (Props) => {
 
+    const {call_time_minutes,call_time_second,billing_term,cost } = Props;
     return (
         <Box sx={{ flexGrow: 1, mx: 'auto', lineHeight: 7, textAlign: 'center'}}>
                 <ViewResult>{Props.cost}</ViewResult>
--- src/Components/Forms/DefaultCalcForm.js
+++ src/Components/Forms/DefaultCalcForm.js
@@ -3,27 +3,27 @@ import {
     Button,
     Stack,
     Grid,
-    FormLabel
+    FormLabel,
+    getInputAdornmentUtilityClass,
 } from '@mui/material';
+import {useState} from "react";
 import { useForm } from "react-hook-form";
 import { TextItem,CalcExec } from '../Styleds/Box'
 import Display from '../Displays/CalcResultDisplay'
 
-const DefaultCalcForm = (Props) => {
+const DefaultCalcForm = () => {
 
-    const result = 230;
-
+    const [setCost,setCostProp] = useState(0);
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = inputData => calcs(inputData);
-
-    const calcs = data => {
-        const callTime = Number(data.call_time_minutes * 60)+Number(data.call_time_second);
-        result = (callTime/data.billing_term)*data.cost;
+    const onSubmit = inputData => {
+        const callTime = Number(inputData.call_time_minutes * 60)+Number(inputData.call_time_second);
+        const cost = (callTime/inputData.billing_term)*inputData.cost;
+        setCostProp(Number(cost));
     };
 
     return (
         <div>
-        <Display cost={result} />
+        <Display cost={setCost} />
         <Stack component="form" autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
             <TextItem mx="auto">
                 <Grid container spacing={2}>
@@ -44,14 +44,13 @@ const DefaultCalcForm = (Props) => {
                         <TextField {...register("billing_term", { required: true })} size="small" />
                     </Grid>
                     <Grid item xs={6}>
-                        <FormLabel>料金</FormLabel>
+                        <FormLabel>料金/(秒)</FormLabel>
                         <TextField {...register("cost", { required: true })} size="small" />
                     </Grid>
                 </Grid>
             </TextItem>
-            <CalcExec type="submit" variant="contained" style={{width: '230px',margin: '0 auto',marginTop: '50px'}}>計算するよ</CalcExec>
-        </Stack>
-
+            <CalcExec type="submit" variant="contained" style={{width: '230px',margin: '0 auto',marginTop: '50px'}}>計算</CalcExec>
+        </Stack> 
         </div>
     );
 };
